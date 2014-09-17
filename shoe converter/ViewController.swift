046d3c1288c8445ca08e46e3b09721ed7dcb1985
Keviv vivek <vivekpatna52@gmail.com>
2014-09-17 15:15:11 +0530

    added women shoe size converter

--- shoe converter/ViewController.swift
+++ shoe converter/ViewController.swift
@@ -11,14 +11,23 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var menShoeSizeTextField: UITextField!
+    @IBOutlet weak var womenShoeSizeTextField: UITextField!
     
     @IBOutlet weak var convertedLabel: UILabel!
+    @IBOutlet weak var womenShoeConvertedLabel: UILabel!
     
     @IBAction func convertButtonPressed(sender: UIButton) {
         var sizeFromTextField = menShoeSizeTextField.text.toInt()!
         let conversionConstant = 30
         convertedLabel.hidden = false
         convertedLabel.text = "European Shoe Size = \(sizeFromTextField + conversionConstant)"
+    }
+    
+    @IBAction func womenShoeConvertButtonPressed(sender: AnyObject) {
+        var sizeFromTextField = Double((womenShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        womenShoeConvertedLabel.hidden = false
+        womenShoeConvertedLabel.text = "European Shoe Size = \(sizeFromTextField + conversionConstant)"
     }
     
     override func viewDidLoad() {
